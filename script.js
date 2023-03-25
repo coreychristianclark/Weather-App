99308c19cca0704464d904aad09cd7b58db32140
Corey Clark <coreychristianclark@gmail.com>
2023-03-25 11:09:45 -0400

    Added a console.log snippet.

--- script.js
+++ script.js
@@ -16,6 +16,9 @@ let weather = {
         const { speed } = data.wind;
             console.log(name, icon, description, temp, humidity, speed);
             document.querySelector(".city").innerText = "Weather in " + name;
-            document.querySelector(".icon").src=""
+            document.querySelector(".icon").src = "
+            
     }
 }
+
+
