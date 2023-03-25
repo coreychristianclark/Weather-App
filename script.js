a3ce509e790ef87d6e3e3db32cf9c8a02b0f8de0
Corey Clark <coreychristianclark@gmail.com>
2023-03-24 23:11:58 -0400

    Added to JS.

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 let weather = {
     apiKey: "a0dbcb0d4363c843bc8ea65ec18118bc",
-    fetchWeather: function (city) {
+    fetchWeather: function(city) {
         fetch("https://api.openweathermap.org/data/2.5/weather?q="
             + city
-            + "&appid&units=imperial&appid="
+            + "&units=imperial&appid="
             + this.apiKey
+
         ).then((response) => response.json())
-            .then((data) => console.log(data));
+            .then((data) => this.displayWeather(data));
+    },
+        displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+            console.log(name, icon, description, temp, humidity, speed);
+            document.querySelector(".city").innerText = "Weather in " + name;
+            document.querySelector(".icon").src=""
     }
-displayWeather: function (data) {
-
-    }
-
 }
